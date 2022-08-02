f90c98ddbe08fbd126e8555873d77c74aeb801a3
niyonkuru1 <sylvainniyonkuru2@gmail.com>
2022-08-02 21:01:08 +0200

    feat(admin-crud-with-databases-setup-1): Adding and configure the heroku to allow the app to be deployed to heroku and configure the github
                    - Add the app to the heroku and configure the postgres database to use the hosted database on heroku
                    - Add the github workflow to automate the deployment on the push. ( trigger deployment on pushing to the specified remote branch
    
                [WIP #1]

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { ApolloServer } from "apollo-server";
-import { context } from "./context";  
+import { context } from "./context"; 
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core"; 
 
 import { schema } from "./schema";
 export const server = new ApolloServer({
-    schema,
-    context
-})
+  schema,
+  context,
+  plugins: [ApolloServerPluginLandingPageLocalDefault()],
+});
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 server.listen({port}).then(({url})=> console.log(`The server started at ${url}`));
